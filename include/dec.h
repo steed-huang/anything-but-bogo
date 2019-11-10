d1a872af9c095c643302a50663782a72a597147d
Steed Huang <steedhuang2002@gmail.com>
2019-11-10 15:25:24 -0500

    improved organizational structure and added isSorted method

--- include/dec.h
+++ include/dec.h
@@ -6,7 +6,22 @@ class Sort
 {
 public:
     Sort(int vectorLen); // initializes vector of random numbers
-    void printNums();    // prints nums vector
+
+    // general methods
+    void printNums(); // prints nums vector
+    bool isSorted();  // whether nums is sorted
+
+    // sorting algorithms
+    void quickSort();
+    void mergeSort();
+    void heapSort();
+    void shellSort();
+    void insertionSort();
+    void selectionSort();
+    void bubbleSort();
+    void miracleSort();
+
+    // algorithm assisting methods
 
 private:
     std::vector<int> nums;
--- src/def.cpp
+++ src/def.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include <ctime>
 
+/*CONSTRUCTOR*/
 Sort::Sort(int vectorLen)
 {
     // populates nums vector with vectorLen random ints: [1,100]
@@ -11,9 +12,26 @@ Sort::Sort(int vectorLen)
         nums.push_back(distr(mers)); // populate vector
 }
 
+/*GENERAL METHODS*/
 void Sort::printNums()
 {
     // iterates through nums vector and prints contents
     for (std::vector<int>::const_iterator i = nums.begin(); i != nums.end(); ++i)
         std::cout << *i << ' ';
 }
+
+bool Sort::isSorted()
+{
+    // iterates through nums vector and determines whether its sorted
+    for (int i = 0; i < nums.size() - 1; ++i)
+        if (nums[i + 1] < nums[i])
+            return false;
+    return true;
+}
+
+/*SORTING ALGORITHMS*/
+void Sort::quickSort()
+{
+}
+
+/*ALGORITHM ASSISTING FUNCTIONS*/
